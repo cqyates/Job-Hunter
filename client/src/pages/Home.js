8f188a5653ebbf21eb4fc643fc87ef7adddf1599
Corey Yates <cqyates@gmail.com>
2023-09-20 21:36:23 -0400

    implemented ReactRouter

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+const HomePage = () => {
+    return (
+        <>
+        <h3>Welcome to Job Hunter</h3>
+        <h4>A Shameless Attempt to Demonstrate My React Skills While Organizing My Jobs</h4>
+        </>
+    )
+}
+
+export default HomePage
