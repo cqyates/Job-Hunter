178f5f43999f509c49c065b199d7304541106563
Corey Yates <cqyates@gmail.com>
2023-09-21 12:03:51 -0400

    moved login to modal

--- client/src/index.js
+++ client/src/index.js
@@ -22,11 +22,11 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: 'profile/:id',
+        path: 'profile',
         element: <ProfilePage />,
       },
       {
-        path: 'project/:id',
+        path: 'project',
         element: <ProjectPage />,
       },
       {
