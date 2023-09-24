98f750e53bf438bca82c78003df3c643ec61dcd8
Corey Yates <cqyates@gmail.com>
2023-09-24 19:00:48 -0400

    profile page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 
 const HomePage = () => {
+    const [user, setUser] = useState({})
     return (
         <>
         <h3>Welcome to Job Hunter</h3>
--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -1,7 +1,8 @@
 const router=require("express").Router();
-const {createUser, loginUser} = require("../../controllers/user-controller.js")
+const {createUser, loginUser, getMe} = require("../../controllers/user-controller.js")
 //works, tested with insomnia
 router.route("/").post(createUser);
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
+router.route("/me").get(getMe)
 
 module.exports=router
